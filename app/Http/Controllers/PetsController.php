53823b490a257333c220d92af2e2afcfd271378b
arwa faizee <arwafaizee142@gmail.com>
2024-04-26 13:53:28 +0530

    petprofilechanges

--- app/Http/Controllers/PetsController.php
+++ app/Http/Controllers/PetsController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\PetTypes;
 use App\Models\PetBreeds;
+
 use Illuminate\Http\Request;
 use App\Models\Pets;
+use Inertia\Inertia;
 class PetsController extends Controller
 {
     //
@@ -24,4 +26,20 @@ class PetsController extends Controller
             'pet' => $pet,
         ]);
     }
+    public function getPetGender()
+    {
+        $pet_gender = config('variables.petGender');
+        return response()->json($pet_gender);
+    }
+
+    public function getPetTypes(){
+        $pet_types = PetTypes::all();
+        return response()->json($pet_types);
+    }
+
+    public function getPetBreeds(){
+        $pet_breeds = PetBreeds::all();
+        return response()->json($pet_breeds);
+    }
+    
 }
