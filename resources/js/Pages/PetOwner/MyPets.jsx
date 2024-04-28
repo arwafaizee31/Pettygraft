cff8b6b5e2ac8429bbcfe9455659c016a6d08cc0
NishthaBordia <bordianishtha@gmail.com>
2024-04-28 14:24:46 +0530

    owner side my pets page completed

--- resources/js/Pages/PetOwner/MyPets.jsx
+++ resources/js/Pages/PetOwner/MyPets.jsx
@@ -3,8 +3,11 @@ import NavBar from "@/components/NavBar";
 import OwnerMyPetsCard from "@/components/OwnerMyPetsCard";
 import { useEffect, useState } from "react";
 import axios from "axios"; // Import axios for making HTTP requests
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import PrimaryButton from "@/components/PrimaryButton";
 
-export default function MyPets({ auth }) {
+export default function MyPets({ auth, link }) {
     const [ownerMyPets, setownerMyPets] = useState([]);
     useEffect(() => {
         fetch(`/api/ownerMyPets/${auth.user.id}`)
@@ -19,14 +22,36 @@ export default function MyPets({ auth }) {
     return (
         <>
             <NavBar></NavBar>
-            <div className="OwnerMyPetsCardContainer ">
+            <div className="text-5xl myPetsHeading">
+                <strong>My Pets </strong>
+            </div>
+            <div className="OwnerMyPetsCardContainer">
                 <div className="row">
-                    <div className="grid grid-cols-4 gap-4">
+                    <div
+                        className={`grid ${
+                            ownerMyPets.length === 1
+                                ? "md:grid-cols-1 md:gap-1 col-span-1 lg:grid-cols-3 lg:gap-3 lg:col-span-3"
+                                : "lg:grid-cols-3 lg:gap-3 md:grid-cols-2 md:gap-2 sm:grid-cols-12 "
+                        }`}
+                    >
                         {ownerMyPets.map((pet) => (
-                            <OwnerMyPetsCard key={pet.id} pet={pet} />
+                            <div
+                                className={`grid ${
+                                    ownerMyPets.length === 1
+                                        ? "lg:col-start-2 md:col-start-1"
+                                        : ""
+                                }`}
+                            >
+                                <OwnerMyPetsCard key={pet.id} pet={pet} />
+                            </div>
                         ))}
                     </div>
                 </div>
+            </div>
+            <div className="addMorePets">
+                <PrimaryButton>
+                    <a href="/petowner/petRegistration">Add more pets</a>
+                </PrimaryButton>
             </div>
         </>
     );
